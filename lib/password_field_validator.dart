ec76550e3aec3c2cb5341c5aca0ca0e381515c97
Pierre-Monier <pierre.monier.dev@gmail.com>
2024-05-18 20:58:36 +0200

    feat: add onValid and onInvalid

--- lib/password_field_validator.dart
+++ lib/password_field_validator.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:password_field_validator/components/validator_item.dart';
 import 'package:password_field_validator/validator/enum_valdation.dart';
-
 import 'package:password_field_validator/validator/validator.dart';
 import 'package:password_field_validator/validator/validator_message.dart';
 
 class PasswordFieldValidator extends StatefulWidget {
-  final int minLength;
-  final int uppercaseCharCount;
-  final int lowercaseCharCount;
-  final int numericCharCount;
-  final int specialCharCount;
+  final int? minLength;
+  final int? uppercaseCharCount;
+  final int? lowercaseCharCount;
+  final int? numericCharCount;
+  final int? specialCharCount;
 
   final Color defaultColor;
   final Color successColor;
@@ -23,70 +22,91 @@ class PasswordFieldValidator extends StatefulWidget {
   final String? numericCharMessage;
   final String? specialCharacterMessage;
 
-  const PasswordFieldValidator(
-      {Key? key,
-      required this.minLength,
-      required this.uppercaseCharCount,
-      required this.lowercaseCharCount,
-      required this.numericCharCount,
-      required this.specialCharCount,
-      required this.defaultColor,
-      required this.successColor,
-      required this.failureColor,
-      required this.controller,
-      this.minLengthMessage,
-      this.uppercaseCharMessage,
-      this.lowercaseMessage,
-      this.numericCharMessage,
-      this.specialCharacterMessage})
-      : super(key: key);
+  final VoidCallback? onValid;
+  final VoidCallback? onInvalid;
+
+  const PasswordFieldValidator({
+    Key? key,
+    this.minLength,
+    this.uppercaseCharCount,
+    this.lowercaseCharCount,
+    this.numericCharCount,
+    this.specialCharCount,
+    required this.defaultColor,
+    required this.successColor,
+    required this.failureColor,
+    required this.controller,
+    this.minLengthMessage,
+    this.uppercaseCharMessage,
+    this.lowercaseMessage,
+    this.numericCharMessage,
+    this.specialCharacterMessage,
+    this.onValid,
+    this.onInvalid,
+  }) : super(key: key);
 
   @override
   _PasswordFieldValidatorState createState() => _PasswordFieldValidatorState();
 }
 
 class _PasswordFieldValidatorState extends State<PasswordFieldValidator> {
-  final Map<Validation, bool> _selectedCondition = {
-    Validation.atLeast: false,
-    Validation.uppercase: false,
-    Validation.lowercase: false,
-    Validation.numericCharacter: false,
-    Validation.specialCharacter: false,
+  late final Map<Validation, bool> _selectedCondition = {
+    if (widget.minLength != null) Validation.atLeast: false,
+    if (widget.uppercaseCharCount != null) Validation.uppercase: false,
+    if (widget.lowercaseCharCount != null) Validation.lowercase: false,
+    if (widget.numericCharCount != null) Validation.numericCharacter: false,
+    if (widget.specialCharCount != null) Validation.specialCharacter: false,
   };
 
   late bool isFirstRun;
 
   void validate() {
-    _selectedCondition[Validation.atLeast] = Validator().hasMinimumLength(
-      widget.controller.text,
-      widget.minLength,
-    );
+    if (widget.minLength != null) {
+      _selectedCondition[Validation.atLeast] = Validator().hasMinimumLength(
+        widget.controller.text,
+        widget.minLength!,
+      );
+    }
 
-    _selectedCondition[Validation.uppercase] = Validator().hasMinimumUppercase(
-      widget.controller.text,
-      widget.uppercaseCharCount,
-    );
+    if (widget.uppercaseCharCount != null) {
+      _selectedCondition[Validation.uppercase] =
+          Validator().hasMinimumUppercase(
+        widget.controller.text,
+        widget.uppercaseCharCount!,
+      );
+    }
 
-    _selectedCondition[Validation.lowercase] = Validator().hasMinimumLowercase(
-      widget.controller.text,
-      widget.lowercaseCharCount,
-    );
+    if (widget.lowercaseCharCount != null) {
+      _selectedCondition[Validation.lowercase] =
+          Validator().hasMinimumLowercase(
+        widget.controller.text,
+        widget.lowercaseCharCount!,
+      );
+    }
 
-    _selectedCondition[Validation.numericCharacter] =
-        Validator().hasMinimumNumericCharacters(
-      widget.controller.text,
-      widget.numericCharCount,
-    );
+    if (widget.numericCharCount != null) {
+      _selectedCondition[Validation.numericCharacter] =
+          Validator().hasMinimumNumericCharacters(
+        widget.controller.text,
+        widget.numericCharCount!,
+      );
+    }
 
-    _selectedCondition[Validation.specialCharacter] =
-        Validator().hasMinimumSpecialCharacters(
-      widget.controller.text,
-      widget.specialCharCount,
-    );
+    if (widget.specialCharCount != null) {
+      _selectedCondition[Validation.specialCharacter] =
+          Validator().hasMinimumSpecialCharacters(
+        widget.controller.text,
+        widget.specialCharCount!,
+      );
+    }
 
-    setState(() {
-      return;
-    });
+    setState(() {});
+
+    if (_selectedCondition.values.every((condition) => condition)) {
+      widget.onValid?.call();
+    } else {
+      widget.onInvalid?.call();
+    }
   }
 
   @override
@@ -106,32 +126,35 @@ class _PasswordFieldValidatorState extends State<PasswordFieldValidator> {
       children: _selectedCondition.entries.map((entry) {
         int conditionValue = 0;
         String conditionMessage = '';
-        if (entry.key == Validation.atLeast) {
-          conditionValue = widget.minLength;
+        if (entry.key == Validation.atLeast && widget.minLength != null) {
+          conditionValue = widget.minLength!;
           conditionMessage = widget.minLengthMessage ??
               validatorMessage.entries
                   .firstWhere((element) => element.key == Validation.atLeast)
                   .value
                   .toString();
         }
-        if (entry.key == Validation.uppercase) {
-          conditionValue = widget.uppercaseCharCount;
+        if (entry.key == Validation.uppercase &&
+            widget.uppercaseCharCount != null) {
+          conditionValue = widget.uppercaseCharCount!;
           conditionMessage = widget.uppercaseCharMessage ??
               validatorMessage.entries
                   .firstWhere((element) => element.key == Validation.uppercase)
                   .value
                   .toString();
         }
-        if (entry.key == Validation.lowercase) {
-          conditionValue = widget.lowercaseCharCount;
+        if (entry.key == Validation.lowercase &&
+            widget.lowercaseCharCount != null) {
+          conditionValue = widget.lowercaseCharCount!;
           conditionMessage = widget.lowercaseMessage ??
               validatorMessage.entries
                   .firstWhere((element) => element.key == Validation.lowercase)
                   .value
                   .toString();
         }
-        if (entry.key == Validation.numericCharacter) {
-          conditionValue = widget.numericCharCount;
+        if (entry.key == Validation.numericCharacter &&
+            widget.numericCharCount != null) {
+          conditionValue = widget.numericCharCount!;
           conditionMessage = widget.numericCharMessage ??
               validatorMessage.entries
                   .firstWhere(
@@ -139,8 +162,9 @@ class _PasswordFieldValidatorState extends State<PasswordFieldValidator> {
                   .value
                   .toString();
         }
-        if (entry.key == Validation.specialCharacter) {
-          conditionValue = widget.specialCharCount;
+        if (entry.key == Validation.specialCharacter &&
+            widget.specialCharCount != null) {
+          conditionValue = widget.specialCharCount!;
           conditionMessage = widget.specialCharacterMessage ??
               validatorMessage.entries
                   .firstWhere(
